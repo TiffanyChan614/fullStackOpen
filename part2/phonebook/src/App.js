222a6b1f1a54307c456d65d69529a1796a87b3dc
TiffChan <tiffanychan1999614@gmail.com>
2023-06-24 14:05:07 -0400

    Finish 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import AddForm from './components/AddForm'
 import Numbers from './components/Numbers'
+import axios from 'axios'
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: 'Arto Hellas', number: '040-1234567' },
-	])
+	const [persons, setPersons] = useState([])
 	const [newName, setNewName] = useState('')
 	const [newNumber, setNewNumber] = useState('')
 	const [search, setSearch] = useState('')
 	const [shownPersons, setShownPersons] = useState(persons)
+
+	useEffect(() => {
+		axios.get('http://localhost:3002/persons').then((response) => {
+			setPersons(response.data)
+			setShownPersons(response.data)
+		})
+	}, [])
 
 	const handleChangeName = (event) => {
 		setNewName(event.target.value)
