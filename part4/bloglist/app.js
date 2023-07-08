32fc876e0c71f09f39e5f3cecd7abbf590922c91
TiffChan <tiffanychan1999614@gmail.com>
2023-07-08 17:25:27 -0400

    Separate config and model from app

---
+++ part4/bloglist/app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const Blog = require('./models/blog')
+
+mongoose.connect(config.MONGODB_URL)
+
+console.log('connected to MongoDB')
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+module.exports = app
